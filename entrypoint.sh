0328c4be8336e5be8ca5d0c4d7f6ed39d74bdf72
linleizhou <linleizhou19910924@163.com>
2018-11-02 17:00:25 +0800

    INIT

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 case "$1" in
 
   'master')
-  	ARGS="-ip `hostname -i` -mdir /data"
+  	ARGS="-mdir /data"
 	# Is this instance linked with an other master? (Docker commandline "--link master1:master")
 	if [ -n "$MASTER_PORT_9333_TCP_ADDR" ] ; then
 		ARGS="$ARGS -peers=$MASTER_PORT_9333_TCP_ADDR:$MASTER_PORT_9333_TCP_PORT"
@@ -12,7 +12,7 @@ case "$1" in
 	;;
 
   'volume')
-  	ARGS="-ip `hostname -i` -dir /data"
+  	ARGS=" -dir /data"
 	# Is this instance linked with a master? (Docker commandline "--link master1:master")
   	if [ -n "$MASTER_PORT_9333_TCP_ADDR" ] ; then
 		ARGS="$ARGS -mserver=$MASTER_PORT_9333_TCP_ADDR:$MASTER_PORT_9333_TCP_PORT"
@@ -21,7 +21,7 @@ case "$1" in
 	;;
 
   'server')
-  	ARGS="-ip `hostname -i` -dir /data"
+  	ARGS="-dir /data"
   	if [ -n "$MASTER_PORT_9333_TCP_ADDR" ] ; then
 		ARGS="$ARGS -master.peers=$MASTER_PORT_9333_TCP_ADDR:$MASTER_PORT_9333_TCP_PORT"
 	fi
@@ -29,7 +29,7 @@ case "$1" in
   	;;
 
   'filer')
-  	ARGS="-ip `hostname -i`"
+  	ARGS=""
   	if [ -n "$MASTER_PORT_9333_TCP_ADDR" ] ; then
 		ARGS="$ARGS -master=$MASTER_PORT_9333_TCP_ADDR:$MASTER_PORT_9333_TCP_PORT"
 	fi
